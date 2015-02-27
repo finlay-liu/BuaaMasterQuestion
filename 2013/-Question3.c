28833e8150c7caba3130e9674de9dade47f286bd
finlay-liu <finlaylyz@gmail.com>
2015-02-27 20:31:43 +0800

    Add 2013

---
+++ 2013/-Question3.c
@@ -0,0 +1,13 @@
+// Author : FinlayLiu
+// Time : 2015-02-27
+// Problem3 : 科学计数法
+// Debug : VC6.0
+#include <stdio.h>
+
+int main()
+{
+	freopen("sample3.in", "r", stdin);
+
+	fclose(stdin);
+	return 0;
+}
