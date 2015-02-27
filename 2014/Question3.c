774f49eff757086e81abecab05be7ffaf6135bbf
finlay-liu <finlaylyz@gmail.com>
2015-02-27 13:12:33 +0800

    Add Question3 还没做完

--- 2014/Question3.c
+++ 2014/Question3.c
@@ -2,3 +2,34 @@
 // Time : 2015-02-27 
 // Problem3：排版题
 // Debug : Linux,gcc 
+// Todo : 排版还有问题
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{    
+    int n;          // 冒号位置
+    
+    int bytes_read;
+    int nbytes = 100;
+    char *strs;
+    
+    scanf("%d\n",&n);    
+    
+    strs = (char *) malloc (nbytes + 1);
+    
+    while( getline(&strs, &nbytes, stdin) != -1)
+    {
+        if(*strs == '\n')       // 如果是空行
+            break;
+            
+        char *token1=strtok(strs,":");
+        char *token2=strtok(token1,":");
+        
+        printf("%s%d",token1,n);
+        printf(" : %s\n",token2);
+    } 
+
+    return 0;
+}
